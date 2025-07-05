06d718d2c58b6776122321a281471c06828f5303
Abhishek <41145278+BladeX-1@users.noreply.github.com>
2025-07-05 09:08:29 +0000

    add videos

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ function validateForm() {
 
     return true; // Submit the form
 }
+
+function liveValidate() {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+
+    if (name.value.trim() === "") {
+        if (!name.classList.contains("invalid")) {
+            name.classList.add("invalid");
+        }
+    } else {
+        if (name.classList.contains("invalid")) {
+            name.classList.remove("invalid");
+        }
+    }
+
+    // email validation regex
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailPattern.test(email.value.trim())) {
+        if (!email.classList.contains("invalid")) {
+            email.classList.add("invalid");
+        }
+    } else {
+        if (email.classList.contains("invalid")) {
+            email.classList.remove("invalid");
+        }
+    }
+}
